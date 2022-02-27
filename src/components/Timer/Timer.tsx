d752d7d8217a83b3fd736d0f89c7bcb30463ba4f
aezazali1997 <aezazali.pro@gmail.com>
2022-02-27 13:23:28 +0500

    removed useEffect called

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,5 +1,5 @@
 
-import React,{useEffect, useState} from 'react'
+import React,{ useState} from 'react'
 import './Timer.css'
 import {TimerButton} from '../TimerButton/TimerButton'
 let myInterval:ReturnType<typeof setTimeout>;
